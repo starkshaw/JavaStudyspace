c4f894e4f98aecfec4b43589b2847ef3370cb3b3
starkshaw <ttxzb@hotmail.com>
2015-05-04 15:39:19 +0100

    Added imcompleted Heuristic approximation algorithm.

--- TSP/src/TSP.java
+++ TSP/src/TSP.java
@@ -15,7 +15,10 @@ import java.math.*;
 
 public class TSP {
 
-    final static String currentPath = System.getProperty("user.dir");       // Store current path
+    public final static String currentPath = System.getProperty("user.dir");       // Store current path
+
+    public static boolean[] visited;
+    public static double distance = 0.0;
 
     public static void main(String[] args) {
         BufferedReader preread = null;     // Pre-read the line and column from the file
@@ -59,12 +62,15 @@ public class TSP {
                     coordinates[i][j - 2] = Double.parseDouble(allData[i][j]);
                 }
             }
-            System.out.println("\n#\tTOWN");
+            /*System.out.println("\n#\tTOWN");
             for (int i = 0; i < allData.length; i++) {
                 System.out.println((i + 1) + "\t" + allData[i][1]);
             }
-            System.out.println("\nAccording to the collected data, there are " + amountOfLine + " towns found.");
-            System.out.print("\nEnter the index of your origin and destination, separate by a comma only: ");
+            System.out.println("\nAccording to the collected data, there are " + amountOfLine + " towns found.");*/
+            visited = new boolean[allData.length];      // Check if current town is visited
+            retrieveAllTowns(31, coordinates, allData);
+            System.out.println(distance);
+            /*System.out.print("\nEnter the index of your origin and destination, separate by a comma only: ");
             indexs = input.nextLine().split(",");
             System.out.println("\nCalculating the distance from " + allData[Integer.parseInt(indexs[0]) - 1][1] + " to " + allData[Integer.parseInt(indexs[1]) - 1][1] + "...");
             // Calculations
@@ -73,7 +79,7 @@ public class TSP {
                             coordinates[Integer.parseInt(indexs[0]) - 1][0],
                             coordinates[Integer.parseInt(indexs[0]) - 1][1],
                             coordinates[Integer.parseInt(indexs[1]) - 1][0],
-                            coordinates[Integer.parseInt(indexs[1]) - 1][1]), 3) + " km.");
+                            coordinates[Integer.parseInt(indexs[1]) - 1][1]), 3) + " km.");*/
             // Print out all collected data
             /*for (int i = 0; i < coordinates.length; i++) {
                 for (int j = 0; j < coordinates[i].length; j++) {
@@ -100,8 +106,9 @@ public class TSP {
 
     /**
      * Optimized mathematical round
-     * @param value     The value needs to be rounded.
-     * @param places    The amount of significant digit after decimal point.
+     *
+     * @param value  The value needs to be rounded.
+     * @param places The amount of significant digit after decimal point.
      * @return The rounded value.
      */
     public static double round(double value, int places) {
@@ -110,5 +117,75 @@ public class TSP {
         BigDecimal bd = new BigDecimal(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
         return bd.doubleValue();
+    }
+
+    public static void retrieveAllTowns(int indexOfOrigin, double[][] coordinates, String[][] allDataOfTown) {
+        System.out.print(allDataOfTown[indexOfOrigin - 1][1] + " ~ ");
+        if(visited[indexOfOrigin-1] == false){
+            visited[indexOfOrigin-1]=true;
+            distance += nearestTown(indexOfOrigin,coordinates,allDataOfTown)[1];
+            int currentTown = (int)nearestTown(indexOfOrigin,coordinates,allDataOfTown)[0];
+            retrieveAllTowns(currentTown-1,coordinates,allDataOfTown);
+        } else {
+
+        }
+
+        /*visited[indexOfOrigin - 1] = true;
+        int visitedTown = 1;      // The amount of visited city
+        int indexOfNearestTown = indexOfOrigin;
+        double distanceOfNearestTown;
+        double sum = 0.0;
+        while (visitedTown != visited.length) {
+            System.out.print(allDataOfTown[indexOfNearestTown - 1][1] + " ~ ");
+            if (indexOfNearestTown == indexOfOrigin || visited[indexOfNearestTown - 1] == false) {
+                indexOfNearestTown = (int) nearestTown(indexOfNearestTown, coordinates, allDataOfTown)[0];
+                distanceOfNearestTown = nearestTown(indexOfNearestTown, coordinates, allDataOfTown)[1];
+                System.out.println((allDataOfTown[indexOfNearestTown - 1][1] + "\t" + distanceOfNearestTown));
+                sum += distanceOfNearestTown;
+                visited[indexOfNearestTown - 1] = true;
+                visitedTown++;
+            } else if (indexOfNearestTown != indexOfOrigin && visited[indexOfNearestTown - 1] == true) {
+                indexOfNearestTown = (int) nearestTown(indexOfNearestTown, coordinates, allDataOfTown)[2];
+                distanceOfNearestTown = nearestTown(indexOfNearestTown, coordinates, allDataOfTown)[3];
+                System.out.println((allDataOfTown[indexOfNearestTown - 1][1] + "\t" + distanceOfNearestTown));
+                sum += distanceOfNearestTown;
+                visited[indexOfNearestTown - 1] = true;
+                visitedTown++;
+            }
+        }
+        System.out.println(sum);*/
+        /*for(int i = 0; i<allDataOfTown.length;i++){
+            if(i!=indexOfOrigin-1){
+                System.out.println("Distance from " + allDataOfTown[indexOfOrigin-1][1] + " to " + allDataOfTown[i][1] +
+                " is " + round(DistanceCal.calDistance(
+                        coordinates[indexOfOrigin-1][0],
+                        coordinates[indexOfOrigin-1][1],
+                        coordinates[i][0],
+                        coordinates[i][1]),3) + " km."
+                );
+            }
+        }*/
+    }
+
+    public static double[] nearestTown(int indexOfOrigin, double[][] coordinates, String[][] allDataOfTown) {
+        double[] result = new double[4];
+        result[1] = Integer.MAX_VALUE;
+        for (int i = 0; i < allDataOfTown.length; i++) {
+            if (i != indexOfOrigin - 1 && result[1] > CalDistanceByIndex(indexOfOrigin, i + 1, coordinates)) {
+                result[3] = result[1];
+                result[2] = result[0];
+                result[1] = CalDistanceByIndex(indexOfOrigin, i + 1, coordinates);
+                result[0] = i + 1;
+            }
+        }
+        return result;
+    }
+
+    public static double CalDistanceByIndex(int index1, int index2, double[][] coordinates) {
+        return DistanceCal.calDistance(
+                coordinates[index1 - 1][0],
+                coordinates[index1 - 1][1],
+                coordinates[index2 - 1][0],
+                coordinates[index2 - 1][1]);
     }
 }
