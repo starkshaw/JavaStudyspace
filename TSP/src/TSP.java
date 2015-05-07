a8cca2fc3f42439f06891ac35c7a5a69851d9afb
starkshaw <ttxzb@hotmail.com>
2015-05-07 15:44:30 +0100

    Added comments.

--- TSP/src/TSP.java
+++ TSP/src/TSP.java
@@ -64,9 +64,10 @@ public class TSP {
                 System.out.println((i + 1) + "\t" + allData[i][1]);
             }*/
             System.out.println("\nAccording to the collected data, there are " + amountOfLine + " positions found.\n");
-            double[] shortest = findShortestOverall(coordinates, allData);
-            System.out.println("\nStart from " + allData[(int) shortest[0]][1] + "\n");
-            double distance = findPath((int) shortest[0] + 1, coordinates, allData);
+            //double[] shortest = findShortestOverall(coordinates, allData);
+            //System.out.println("\nStart from " + allData[(int) shortest[0]][1] + "\n");
+            //double distance = findPath((int) shortest[0] + 1, coordinates, allData);
+            double distance = findPath(286, coordinates, allData);
             System.out.println("Total distance: " + round(distance, 3) + " km");
         } catch (IOException ex) {      // Exception handlers
             System.out.println("Error.");
@@ -107,28 +108,27 @@ public class TSP {
      * @param allDataOfTown The array stores index, town name, etc.
      */
     public static double findPath(int indexOfOrigin, double[][] coordinates, String[][] allDataOfTown) {
-        boolean[] visited;
-        visited = new boolean[allDataOfTown.length];      // Check if current town is visited
+        boolean[] visited = new boolean[allDataOfTown.length];      // Check if current town is visited
         // Initialize the array
         for (boolean var : visited) {
             var = false;
         }
-        double distance = 0.0;
-        int indexOfNext = indexOfOrigin - 1;
-        visited[indexOfOrigin - 1] = true;
+        double distance = 0.0;  // Accumulative distance
+        int indexOfNext = indexOfOrigin - 1;    // Store the origin as "next"
+        visited[indexOfOrigin - 1] = true;      // Set origin as visited
         double distanceOfNext;
         for (int i = 0; i < allDataOfTown.length - 1; i++) {
             //System.out.print(allDataOfTown[indexOfNext][1] + " ~ ");
-            //System.out.print(allDataOfTown[indexOfNext][0] + ".");  // Output for airport problem
-            double[][] nearest = nearestTown(indexOfNext + 1, coordinates, allDataOfTown);
-            double[] result = findFirstUnvisited(nearest, visited);
-            indexOfNext = (int) result[0];
+            System.out.print(allDataOfTown[indexOfNext][0] + ".");  // Output for airport problem
+            double[][] nearest = nearestTown(indexOfNext + 1, coordinates, allDataOfTown);      // Find routes ordered by distance
+            double[] result = findFirstUnvisited(nearest, visited); // Find the nearest unvisited position
+            indexOfNext = (int) result[0];  // Set next target position
             distanceOfNext = result[1];
             //System.out.println(allDataOfTown[indexOfNext][1] + ": " + round(distanceOfNext, 3) + " km");
-            distance += distanceOfNext;
-            visited[indexOfNext] = true;
+            distance += distanceOfNext;     // Accumulate
+            visited[indexOfNext] = true;    // Set current as visited
         }
-        //System.out.println(indexOfNext + 1);  // Output for airport problem
+        System.out.println(indexOfNext + 1);  // Output for airport problem
         return distance;
     }
 
@@ -153,6 +153,7 @@ public class TSP {
         /*for (int i = 0; i < result.length; i++) {
             System.out.println(allDataOfTown[indexOfOrigin - 1][1] + " ~ " + allDataOfTown[(int) result[i][0]][1] + ":\t" + result[i][1]);
         }*/
+        // Sort
         for (int i = 0; i < result.length; i++) {
             for (int j = result.length - 1; j > i; j--) {
                 if (result[j][1] < result[j - 1][1]) {
@@ -170,6 +171,7 @@ public class TSP {
             System.out.println(result[i][0] + "\t" + result[i][1]);
             //System.out.println(allDataOfTown[indexOfOrigin - 1][1] + " ~ " + allDataOfTown[(int) result[i][0]][1] + ":\t" + result[i][1]);
         }*/
+        // Exclude the origin itself
         for (int i = 1; i < result.length; i++) {
             finalResult[i - 1][0] = result[i][0];
             finalResult[i - 1][1] = result[i][1];
@@ -191,7 +193,7 @@ public class TSP {
     public static double[] findFirstUnvisited(double[][] sortedList, boolean[] visited) {
         double result[] = new double[2];
         for (int i = 0; i < sortedList.length; i++) {
-            if (visited[(int) sortedList[i][0]] == false) {
+            if (visited[(int) sortedList[i][0]] == false) {     // If find the first unvisited position, store and jump out the loop
                 result[0] = sortedList[i][0];
                 result[1] = sortedList[i][1];
                 break;
@@ -219,16 +221,23 @@ public class TSP {
                 coordinates[index2 - 1][1]);
     }
 
+    /**
+     * Find the shortest route among all possibilities.
+     *
+     * @param coordinates   The array consists coordinates.
+     * @param allDataOfTown The array stores index, town name, etc.
+     * @return The shortest route.
+     */
     public static double[] findShortestOverall(double[][] coordinates, String[][] allDataOfTown) {
-        double[] finalResult = new double[2];
-        double[] result = new double[allDataOfTown.length];
+        double[] finalResult = new double[2];               // Stores the final (minimum) answer
+        double[] result = new double[allDataOfTown.length]; // All answers
         finalResult[0] = 0.0;
         finalResult[1] = Integer.MAX_VALUE;
         for (int i = 0; i < allDataOfTown.length; i++) {
-            result[i] = findPath(i + 1, coordinates, allDataOfTown);
+            result[i] = findPath(i + 1, coordinates, allDataOfTown);    // Find the route
             System.out.println("Searching on " + (i + 1) + " (" + allDataOfTown[i][1] + "): " + round(result[i], 3) + " km");
         }
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {   // Find the minimum in results
             if (finalResult[1] > result[i]) {
                 finalResult[1] = result[i];
                 finalResult[0] = i;
